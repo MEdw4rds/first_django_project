faf90d8b20754a3a4e5b5a7bb0b733c15d8033f2
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-12-02 13:58:28 +0000

    set debug to false again

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6sr^!o#o0ksi-q7tgu$1796tr7c!2h+5raf0gd^%@!19f+z1%1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-medw4rds-firstdjangopro-phuskxmb9xf.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
